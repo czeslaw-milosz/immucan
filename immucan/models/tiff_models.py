32603b807e95870da325304a8c57924583695aee
Radoslaw Jurczak <radoslawjurczak95@gmail.com>
2022-09-21 01:03:46 +0200

    Add model factory (basic U-net-like)
    Signed-off-by: Radoslaw Jurczak <radoslawjurczak95@gmail.com>

--- immucan/models/tiff_models.py
+++ immucan/models/tiff_models.py
@@ -1,7 +1,54 @@
 from tensorflow import keras
 from keras import layers
 
-augmentation_module = keras.Sequential([
-    layers.RandomFlip(mode="horizontal_and_vertical"),
-    layers.RandomRotation(factor=(-0.5, 0.5))
-])
+
+def get_model(train_size: int, val_size: int, use_batch_norm: bool = False):
+    inputs = keras.Input(shape=train_size)  # expected img size (300, 300, 40)
+
+    # Entry block
+    x = layers.Conv2D(filters=32, kernel_size=3, padding="same")(inputs)
+    if use_batch_norm:
+        x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+    x = layers.Conv2D(filters=32, kernel_size=3, padding="same")(inputs)
+    if use_batch_norm:
+        x = layers.BatchNormalization()(x)
+    x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+    # Blocks 1, 2 are identical apart from the feature depth.
+    for filters in [64, 128]:
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2D(filters, 3, padding="same")(x)
+        if use_batch_norm:
+            x = layers.BatchNormalization()(x)
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2D(filters, 3, padding="same")(x)
+        if use_batch_norm:
+            x = layers.BatchNormalization()(x)
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+    # Second half of the network: upsampling inputs
+    for filters in [128, 64]:
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        if use_batch_norm:
+            x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        if use_batch_norm:
+            x = layers.BatchNormalization()(x)
+
+        x = layers.UpSampling2D(2)(x)
+
+    x = layers.Activation("relu")(x)
+    x = layers.Conv2DTranspose(32, 3, padding="same")(x)
+    if use_batch_norm:
+        x = layers.BatchNormalization()(x)
+
+    x = layers.Conv2D(val_size[-1], 3, padding="valid")(x)
+    outputs = layers.UpSampling2D(2)(x)
+
+    # Define the model
+    model = keras.Model(inputs, outputs)
+    return model
